7553a384bdc8058edff5f531327daeb974579296
ADRIAN0819 <adrian.urbina@utec.edu.pe>
2024-10-08 18:52:50 -0500

    cambios 4/8

--- WalletWise/src/main/java/com/example/walletwise/Presupuesto/domain/Presupuesto.java
+++ WalletWise/src/main/java/com/example/walletwise/Presupuesto/domain/Presupuesto.java
@@ -2,9 +2,11 @@ package com.example.walletwise.Presupuesto.domain;
 
 import com.example.walletwise.Usuario.domain.Usuario;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
+@Data
 @Entity
 public class Presupuesto {
 
@@ -31,62 +33,4 @@ public class Presupuesto {
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
 
-    // Getters y Setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public BigDecimal getMontoTotal() {
-        return montoTotal;
-    }
-
-    public void setMontoTotal(BigDecimal montoTotal) {
-        this.montoTotal = montoTotal;
-    }
-
-    public BigDecimal getGastoActual() {
-        return gastoActual;
-    }
-
-    public void setGastoActual(BigDecimal gastoActual) {
-        this.gastoActual = gastoActual;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getAlerta() {
-        return alerta;
-    }
-
-    public void setAlerta(String alerta) {
-        this.alerta = alerta;
-    }
-
-    public String getPeriodo() {
-        return periodo;
-    }
-
-    public void setPeriodo(String periodo) {
-        this.periodo = periodo;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
 }
--- WalletWise/src/main/java/com/example/walletwise/Presupuesto/dtos/PresupuestoDTO.java
+++ WalletWise/src/main/java/com/example/walletwise/Presupuesto/dtos/PresupuestoDTO.java
@@ -1,7 +1,12 @@
 package com.example.walletwise.Presupuesto.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class PresupuestoDTO {
     private Long id;
     private BigDecimal montoTotal;
@@ -10,64 +15,5 @@ public class PresupuestoDTO {
     private String alerta;       // Condición para emitir alertas
     private String periodo;      // Puede ser "Mensual", "Anual", etc.
     private Long usuarioId;      // Referencia al usuario propietario del presupuesto
-
-    // Getters y Setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public BigDecimal getMontoTotal() {
-        return montoTotal;
-    }
-
-    public void setMontoTotal(BigDecimal montoTotal) {
-        this.montoTotal = montoTotal;
-    }
-
-    public BigDecimal getGastoActual() {
-        return gastoActual;
-    }
-
-    public void setGastoActual(BigDecimal gastoActual) {
-        this.gastoActual = gastoActual;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public String getAlerta() {
-        return alerta;
-    }
-
-    public void setAlerta(String alerta) {
-        this.alerta = alerta;
-    }
-
-    public String getPeriodo() {
-        return periodo;
-    }
-
-    public void setPeriodo(String periodo) {
-        this.periodo = periodo;
-    }
-
-    public Long getUsuarioId() {
-        return usuarioId;
-    }
-
-    public void setUsuarioId(Long usuarioId) {
-        this.usuarioId = usuarioId;
-    }
 }
 
