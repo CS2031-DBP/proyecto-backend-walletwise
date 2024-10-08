9b93bf2681d7126fc050fb3ff3a5261c0d903d53
ADRIAN0819 <adrian.urbina@utec.edu.pe>
2024-10-08 18:49:14 -0500

    mismos cambios para cuenta , cambios 2/8

--- WalletWise/src/main/java/com/example/walletwise/Cuenta/dtos/CuentaDTO.java
+++ WalletWise/src/main/java/com/example/walletwise/Cuenta/dtos/CuentaDTO.java
@@ -1,7 +1,12 @@
 package com.example.walletwise.Cuenta.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class CuentaDTO {
     private Long id;
     private String banco;
@@ -9,56 +14,5 @@ public class CuentaDTO {
     private String tipoCuenta;  // Puede ser "Ahorro", "Corriente", etc.
     private String moneda;      // Ej: "USD", "PEN", etc.
     private Long usuarioId;     // Referencia al usuario propietario de la cuenta
-
-    // Getters y Setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBanco() {
-        return banco;
-    }
-
-    public void setBanco(String banco) {
-        this.banco = banco;
-    }
-
-    public BigDecimal getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(BigDecimal saldo) {
-        this.saldo = saldo;
-    }
-
-    public String getTipoCuenta() {
-        return tipoCuenta;
-    }
-
-    public void setTipoCuenta(String tipoCuenta) {
-        this.tipoCuenta = tipoCuenta;
-    }
-
-    public String getMoneda() {
-        return moneda;
-    }
-
-    public void setMoneda(String moneda) {
-        this.moneda = moneda;
-    }
-
-    public Long getUsuarioId() {
-        return usuarioId;
-    }
-
-    public void setUsuarioId(Long usuarioId) {
-        this.usuarioId = usuarioId;
-    }
 }
 
