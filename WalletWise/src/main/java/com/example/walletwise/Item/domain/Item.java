a862b8aa0a9d9469be63a46abcc6d19cfd5c3aa4
ADRIAN0819 <adrian.urbina@utec.edu.pe>
2024-10-08 18:51:23 -0500

    cambios 3/8

--- WalletWise/src/main/java/com/example/walletwise/Item/domain/Item.java
+++ WalletWise/src/main/java/com/example/walletwise/Item/domain/Item.java
@@ -2,12 +2,13 @@ package com.example.walletwise.Item.domain;
 
 import com.example.walletwise.Transaccion.domain.Transaccion;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
+@Data
 @Entity
 public class Item {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,39 +22,4 @@ public class Item {
     @ManyToOne
     @JoinColumn(name = "transaccion_id", nullable = false)
     private Transaccion transaccion;
-
-    // Getters y Setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public BigDecimal getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(BigDecimal precio) {
-        this.precio = precio;
-    }
-
-    public Transaccion getTransaccion() {
-        return transaccion;
-    }
-
-    public void setTransaccion(Transaccion transaccion) {
-        this.transaccion = transaccion;
-    }
 }
--- WalletWise/src/main/java/com/example/walletwise/Item/dtos/ItemDTO.java
+++ WalletWise/src/main/java/com/example/walletwise/Item/dtos/ItemDTO.java
@@ -1,46 +1,16 @@
 package com.example.walletwise.Item.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public class ItemDTO {
     private Long id;
     private String nombre;       // Nombre del ítem, por ejemplo, "Supermercado"
     private BigDecimal precio;   // Precio del ítem
     private Long transaccionId;  // Referencia a la transacción donde está incluido el ítem
-
-    // Getters y Setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public BigDecimal getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(BigDecimal precio) {
-        this.precio = precio;
-    }
-
-    public Long getTransaccionId() {
-        return transaccionId;
-    }
-
-    public void setTransaccionId(Long transaccionId) {
-        this.transaccionId = transaccionId;
-    }
 }
 
