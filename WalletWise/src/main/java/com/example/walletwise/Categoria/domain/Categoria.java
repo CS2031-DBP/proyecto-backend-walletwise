e4d9dad5a04656bc7fc36f46322e8c921489a79b
ADRIAN0819 <adrian.urbina@utec.edu.pe>
2024-10-08 18:45:28 -0500

    Cambios Categoria

--- WalletWise/src/main/java/com/example/walletwise/Categoria/domain/Categoria.java
+++ WalletWise/src/main/java/com/example/walletwise/Categoria/domain/Categoria.java
@@ -3,12 +3,13 @@ package com.example.walletwise.Categoria.domain;
 import com.example.walletwise.Subcategoria.domain.Subcategoria;
 import com.example.walletwise.Transaccion.domain.Transaccion;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
 
 @Entity
+@Data
 public class Categoria {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,55 +29,5 @@ public class Categoria {
     @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
     private List<Transaccion> transacciones;
 
-    // Getters y Setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public List<Subcategoria> getSubcategorias() {
-        return subcategorias;
-    }
-
-    public void setSubcategorias(List<Subcategoria> subcategorias) {
-        this.subcategorias = subcategorias;
-    }
-
-    public List<Transaccion> getTransacciones() {
-        return transacciones;
-    }
-
-    public void setTransacciones(List<Transaccion> transacciones) {
-        this.transacciones = transacciones;
-    }
 }
 
